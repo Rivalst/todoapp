98134f1cb0b2336bff000c9f9c2488d0731f3777
Rivalst <mr.raybchits@gmail.com>
2023-06-03 18:32:19 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:flutter_sliding_up_panel/flutter_sliding_up_panel.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Color _blackColor = Color(0xFF474747);
-  Color _greyColor = Color(0xFFF0F0F7);
+  Color _greyColor = Color(0xFFB4B4C6);
+
 
   Widget data(String name, String number) {
     return Column(
@@ -113,8 +117,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             Spacer(),
-            if(mark)
-              Icon(Icons.circle, size: 15.0, color: Color(0xFF4AD991)),
+            if (mark) Icon(Icons.circle, size: 15.0, color: Color(0xFF4AD991)),
             if (notifi)
               Padding(
                   padding: const EdgeInsets.all(16.0),
@@ -134,7 +137,8 @@ class _MyHomePageState extends State<MyHomePage> {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(30.0),
-          border: Border.all(color: Color(0xFFFFCA83).withOpacity(0.3), width: 1.0),
+          border:
+              Border.all(color: Color(0xFFFFCA83).withOpacity(0.3), width: 1.0),
           boxShadow: [
             BoxShadow(
               color: Color(0xFFFFCA83).withOpacity(0.1),
@@ -148,11 +152,8 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 26.0),
-              child: Icon(
-                  Icons.done,
-                  color: Color(0xFFFFCA83)
-                ),
-              ),
+              child: Icon(Icons.done, color: Color(0xFFFFCA83)),
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -176,6 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: true,
         body: Column(
       children: [
         SafeArea(
@@ -244,25 +246,26 @@ class _MyHomePageState extends State<MyHomePage> {
         Expanded(
             child: Padding(
           padding: const EdgeInsets.only(top: 4.0),
-          child: Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius:
-                  const BorderRadius.vertical(top: Radius.circular(10.0)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
+          child: Stack(
+            children: [
+              Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        const BorderRadius.vertical(top: Radius.circular(10.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ListView(children: [
                   todoContainer('Meet Ann', timeAlarm: '8:00', notifi: true),
                   todoContainer('Buy The Cook'),
                   todoContainer('Call mom'),
@@ -270,23 +273,54 @@ class _MyHomePageState extends State<MyHomePage> {
                       timeAlarm: '11:00 PM', notifi: true, mark: true),
                   todoContainerDone('Visit the University campus '),
                   todoContainerDone('Buy fruits'),
-                  const Spacer(),
-                  SizedBox(
-                    width: 360,
-                    height: 40,
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(6.0)
-                        ),
-                        labelText: 'What do you need to do?',
-                      ),
-                    ),
-                  )
-                ],
+                ]),
               ),
-            ),
+              Positioned(
+                bottom: 10,
+                right: 0,
+                left: 0,
+                child: SlidingUpPanel(
+                    minHeight: 50,
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(16.0)),
+                    color: Colors.white,
+                    panel: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                            width: 55,
+                            height: 5,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16.0),
+                              color: _blackColor,
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 50.0),
+                            child: Card(
+                              elevation: 5.0,
+                              child: SizedBox(
+                                  width: 360,
+                                  height: 40,
+                                  child: TextField(
+                                      decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'What do you need to do?',
+                                  ))),
+                            ))
+                      ],
+                    ),
+                    collapsed: Align(
+                      alignment: Alignment.center,
+                      child: Text('Drag for add ToDo',
+                          style: GoogleFonts.ubuntu(
+                              color: _blackColor, fontWeight: FontWeight.bold)),
+                    )),
+              )
+            ],
           ),
         )),
       ],
